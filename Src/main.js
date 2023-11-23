b1fed4d8e60e962e808be9d3193c2624929a31d0
Pablo Vacas <pablovacasgarcia@gmail.com>
2023-11-23 16:44:01 +0100

    Terminado header y hero page

--- Src/main.js
+++ Src/main.js
@@ -1,17 +1,30 @@
 window.addEventListener('scroll', function () {
     var header = document.querySelector('header');
-    var title = document.querySelector('.title__name');
+    var title = document.querySelector('.title');
     var menuNav = document.querySelector('.menu__nav');
+    var skillsBar = document.querySelectorAll('.skills--item--bar');
+    var skills = document.querySelector('.skills');
+    var aboutme = document.querySelector('.aboutme');
+    var opinions = document.querySelector('.opinions');
+    
 
-    if (window.scrollY > 200) {
-        header.style.position = 'fixed';
+    if (window.scrollY > header.style.width+200) {
+        header.style.backgroundColor = '#333'
         header.style.padding = '10px 0';
-        title.style.float = 'left';
-        menuNav.style.float = 'right';
     } else {
-        header.style.position = 'relative';
+        header.style.background = 'none'
         header.style.padding = '20px 0';
-        title.style.float = 'none';
-        menuNav.style.float = 'none';
+    }
+
+    if (window.scrollY > skills.getBoundingClientRect().top+1300 && window.scrollY < opinions.getBoundingClientRect().top+1800) {
+        skillsBar.forEach(bar => {
+            bar.style.width="100%"
+        });
+        
+    } else {
+        skillsBar.forEach(bar => {
+            bar.style.width="0%"
+        });
+        
     }
 });
